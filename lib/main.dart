0dafc4fdea53f8f4b940e82d11efa5c1b90f4a9e
João Gabriel <gsantana152@gmail.com>
2021-09-30 10:28:25 -0300

    Added provider to saldo

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:bytebank_modulo_6/model/saldo.dart';
 import 'package:bytebank_modulo_6/screens/dashboard.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-void main() => runApp(BytebankApp());
+void main() => runApp(ChangeNotifierProvider(
+      create: (context) => Saldo(0),
+      child: BytebankApp(),
+    ));
 
 class BytebankApp extends StatelessWidget {
   @override
--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,18 +1,16 @@
-import 'package:bytebank_modulo_6/model/saldo.dart';
-import 'package:bytebank_modulo_6/screens/saldo.dart';
+import 'package:bytebank_modulo_6/screens/saldo_card.dart';
 import 'package:flutter/material.dart';
 
 class Dashboard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Bytebank'),
-        ),
-        body: Align(
-          alignment: Alignment.topCenter,
-          child: SaldoCard(
-            Saldo(20.00),
-          ),
-        ));
+      appBar: AppBar(
+        title: const Text('Bytebank'),
+      ),
+      body: Align(
+        alignment: Alignment.topCenter,
+        child: SaldoCard(),
+      ),
+    );
   }
 }
